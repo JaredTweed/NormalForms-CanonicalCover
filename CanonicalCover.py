36e952e20cd38fba8591f7bb52c7ae7b17eeb645
JaredTweed <59375645+JaredTweed@users.noreply.github.com>
2023-03-15 20:01:11 -0700

    Added comments and formatted output

--- CanonicalCover.py
+++ CanonicalCover.py
@@ -2,78 +2,96 @@ from itertools import combinations, permutations
 
 def get_closure(attributes, fds, doPrint=False):
     # Compute the closure of a set of attributes under a set of functional dependencies
+    
+    # Set the closure to the original set of attributes
     closure = set(attributes)
     changed = True
+    
+    # Repeat until the closure no longer changes
     while changed:
         changed = False
+        # For each functional dependency in the set
         for X, Y in fds:
+            # If the left-hand side of the dependency is a subset of the current closure
             if set(X).issubset(closure):
+                # If the right-hand side is not already in the closure
                 if not set(Y).issubset(closure):
+                    # Add the right-hand side to the closure and mark that the closure has changed
                     closure |= set(Y)
                     changed = True
     
+    # If the doPrint parameter is True, print the closure
     if(doPrint == True):
         print('['+attributes+']+ = '+''.join(closure))
     
+    # Return the final closure
     return closure
 
 
 def fds_to_items(data):
+    # Convert a set of functional dependencies to a string of all the attributes in the dependencies
     letters = set()
     for item in data:
         for letter in item:
             letters.update(set(letter))
     
+    # Return the sorted string of attributes
     return ''.join(sorted(letters))
 
+
 def isSetClosureEqual(fds1, fds2):
+    # Check if the closures of two sets of functional dependencies are equal
     if(fds_to_items(fds1) != fds_to_items(fds2)): return False
     attributes = fds_to_items(fds1)
     
-    
+    # For each subset of attributes, compute the closure using both sets of dependencies
     for length in range(1, len(attributes) + 1):
         for combo in combinations(attributes, length):
             sorted_combo = "".join(sorted(combo))
             
             fds1_closure = get_closure(sorted_combo, fds1)
             fds2_closure = get_closure(sorted_combo, fds2)
+            
+            # If the closures are not equal, return False
             if(fds1_closure != fds2_closure): return False
-    # print('closure achieved')
+    
+    # Otherwise, return True
     return True
     
 
 def find_canonical_cover(fds):
-    # Find the canonical cover of a set of functional dependencies
+    # Loop through each functional dependency in fds and check if the left-hand side (LHS) or right-hand side (RHS) has an extraneous attribute.
     for index, (X, Y) in enumerate(fds):
-        # print(X, Y)
         for A in X:
-            # Check if A is extraneous
-            reduced_X = X.replace(A, '')
-            if(reduced_X != ''):
-                reduced_fds = fds[:index] + [(reduced_X, Y)] + fds[index+1:]
-            else: reduced_fds = fds[:index] + fds[index+1:]
+            # Check if attribute A in the LHS of the FD is extraneous
+            reduced_X = X.replace(A, '') # remove attribute A from the LHS
+            if reduced_X != '':
+                reduced_fds = fds[:index] + [(reduced_X, Y)] + fds[index+1:] # create a new set of FDs without the FD (X -> Y) and add the reduced FD (reduced_X -> Y)
+            else:
+                reduced_fds = fds[:index] + fds[index+1:] # create a new set of FDs without the FD (X -> Y)
             
-            # print(fds, reduced_fds, A)
-
-            if(isSetClosureEqual(fds, reduced_fds) == True):
+            # Check if the closure of the new set of FDs is the same as that of the original set of FDs
+            if isSetClosureEqual(fds, reduced_fds) == True:
                 fds = reduced_fds
-                return False, fds
-                
+                return False, fds # if the closure is the same, the FD (X -> Y) is redundant and can be removed from the set of FDs
+        
         for A in Y:
-            # Check if A is extraneous
-            reduced_Y = Y.replace(A, '')
-            if(reduced_Y != ''):
-                reduced_fds = fds[:index] + [(X, reduced_Y)] + fds[index+1:]
-            else: reduced_fds = fds[:index] + fds[index+1:]
+            # Check if attribute A in the RHS of the FD is extraneous
+            reduced_Y = Y.replace(A, '') # remove attribute A from the RHS
+            if reduced_Y != '':
+                reduced_fds = fds[:index] + [(X, reduced_Y)] + fds[index+1:] # create a new set of FDs without the FD (X -> Y) and add the reduced FD (X -> reduced_Y)
+            else:
+                reduced_fds = fds[:index] + fds[index+1:] # create a new set of FDs without the FD (X -> Y)
             
-            # print(fds, reduced_fds, A)
-
-            if(isSetClosureEqual(fds, reduced_fds) == True):
+            # Check if the closure of the new set of FDs is the same as that of the original set of FDs
+            if isSetClosureEqual(fds, reduced_fds) == True:
                 fds = reduced_fds
-                return False, fds
-    return True, fds
+                return False, fds # if the closure is the same, the FD (X -> Y) is redundant and can be removed from the set of FDs
+    
+    return True, fds # if no extraneous attribute was found in any of the FDs, the set of FDs is the canonical cover
 
 def call_canonical_cover(fds):
+    # Call the find_canonical_cover function until no further changes are made
     isFinished = False
     while (isFinished == False):
         isFinished, fds = find_canonical_cover(fds)
@@ -81,6 +99,7 @@ def call_canonical_cover(fds):
     return fds
     
 def all_canonical_cover(fds):
+    # Find all possible canonical covers for a set of functional dependencies
     fds_list = []
     for permutation in permutations(fds):
         perm = list(permutation)
@@ -94,12 +113,11 @@ def all_canonical_cover(fds):
 # fds = [('AB', 'C'), ('C', 'D'), ('BD', 'E'), ('E', 'A'), ('A','C')]
 # fds = [('A','BC'), ('B','C'), ('A','B'), ('AB','C')]
 fds = [('A', 'B'), ('A','C'), ('B','A'), ('B','C'), ('C', 'A'), ('C', 'B')]
-
+print('Input:')
+print(fds)
+print('---\nAll Canonical Covers:')
 all_canonical_cover(fds)
-print('---')
-canonical_cover = call_canonical_cover(fds)
-print(canonical_cover)
-# get_closure('A', fds, doPrint=True)
-# get_closure('B', fds, doPrint=True)
-# get_closure('C', fds, doPrint=True)
+print('---\nClosure:')
+for i in fds_to_items(fds):
+    get_closure(i, fds, doPrint=True)
 
